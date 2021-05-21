caa402ca9e875800fc666998fb75f2810729f145
muhammadzourdy <zhafarmuhammad12@gmail.com>
2021-05-21 19:30:31 +0700

    Boiletplate: next.js and tailwind css

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Amazon 2.0</title>
+        <title>Next.js and Tailwind CSS Boilerplate </title>
       </Head>
 
       {/* ---- TO BEGIN, delete this section and GET CODING!!! ---- */}
       <center className="grid place-items-center mt-10">
-        <h1 className="text-5xl">Lets build Amazon 2.0</h1>
-        <h2>This is your starter template!</h2>
+        <h1 className="text-5xl">Boilerplate Development</h1>
+        <h2>Next.js, Tailwind CSS and Webpack!</h2>
         <br />
         <h3 className="font-bold">
           We will be using Next.js / Tailwind CSS / Redux / Firebase / NextAuth
@@ -18,22 +18,20 @@ export default function Home() {
         <i>(Dont worry, its all setup and ready to use!)</i>
         <h4>Get Ready, Get Set, GO!!!</h4>
 
-        <h5 className="mb-10">#PAPAFAM</h5>
+        <h5 className="mb-10">#ZOURDYTHEDEV</h5>
 
         <div className="bg-red-300 p-10">
-          <p className="font-bold">
-            Dont forget to register for the challenge here!
-          </p>
+          <p className="font-bold">Dont forget to follow my Twitter</p>
           <p>👇👇👇</p>
           <a
-            href="https://www.papareact.com/secret-challenge"
+            href="https://www.twitter.com/zourdythedev"
             className="text-blue-400 underline p-3 font-bold"
           >
-            CLICK HERE TO REGISTER NOW
+            CLICK HEREEE!!!
           </a>
         </div>
 
-        <p className="mt-24">Built with 💙 by Sonny Sangha (PAPA REACT)</p>
+        <p className="mt-24">Built with 💙 by Muhammad Zourdy</p>
       </center>
       {/* ---- ---- */}
     </div>
